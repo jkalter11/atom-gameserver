9907278092acd1c0231bc044db29222979994140
Vladimir Stolyarov <xakep6666@gmail.com>
2016-10-07 18:59:32 +0300

    add leave testing to matchmaker test

--- src/test/java/matchMakerTests/SinglePlayerMatchMakerTest.java
+++ src/test/java/matchMakerTests/SinglePlayerMatchMakerTest.java
@@ -2,11 +2,9 @@ package matchMakerTests;
 
 import matchmaker.MatchMaker;
 import matchmaker.SinglePlayerMatchMaker;
-import model.GameConstants;
 import model.Player;
-import org.junit.Test;
-
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author Alpi
@@ -20,5 +18,8 @@ public class SinglePlayerMatchMakerTest {
 
     Assert.assertEquals(1, singlePlayerMatchMaker.getActiveGameSessions().size());
     Assert.assertNotNull(singlePlayerMatchMaker.getActiveGameSessions().get(0));
+
+    singlePlayerMatchMaker.leaveGame(player);
+    Assert.assertEquals(0, singlePlayerMatchMaker.getActiveGameSessions().size());
   }
 }
