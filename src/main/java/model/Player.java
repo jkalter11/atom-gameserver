fa953810f89721a769ce081b4102dabd9b692e62
Vladimir Stolyarov <xakep6666@gmail.com>
2016-10-11 18:17:05 +0300

    Add some fields to Player

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -4,6 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 /**
  * Server player avatar
  * <a href="https://atom.mail.ru/blog/topic/update/39/">HOMEWORK 1</a> example game instance
@@ -16,6 +19,13 @@ public class Player {
   @NotNull
   private String name;
 
+  private int cellsEaten;
+  private LocalDateTime startTime;
+  private Duration leaderboardTime;
+  private int foodEaten;
+  private int highestMass;
+  private int topLeaderboardPosition;
+
   //TODO maybe we need something else here?
 
   /**
@@ -24,6 +34,7 @@ public class Player {
    * @param name        visible name
    */
   public Player(@NotNull String name) {
+    this.startTime = LocalDateTime.now();
     this.name = name;
     if (log.isInfoEnabled()) {
       log.info(toString() + " created");
